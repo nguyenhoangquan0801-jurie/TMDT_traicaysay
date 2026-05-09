87cf466c3fb80be0c9e7063c7376cdcac4c0c60a
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2026-05-10 01:37:35 +0700

    Add files via upload

---
+++ src/main/java/vn/edu/nlu/fit/nonglamfood/NonglamfoodApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu.fit.nonglamfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonglamfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonglamfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/nlu/fit/nonglamfood/NonglamfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu.fit.nonglamfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonglamfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
